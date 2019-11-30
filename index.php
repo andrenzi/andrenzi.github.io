1f70640df9b5ad1b278ae917e1904a9ba29d4bc1
Andrey <51814226+andrenzi@users.noreply.github.com>
2019-11-30 20:08:40 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Aldrich&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header class=top_header>
+<nav class="nav">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 offset-0 offset-sm-3 offset-md-0 col-sm-6 col-md-4 d-flex">
+                <img src="img/nav_phone.png" class="nav_phone">
+                <h3 class="nav_item" ><a href="tel: +79013028282"> +7 901 302 82 82</a></h3>
+            </div>
+            <div class="col-12 offset-0 col-sm-10 offset-md-1 col-md-6 d-flex">
+             <img src="img/nav_adress.png" class="nav_adress">
+                <h3 class="nav_item">ул.Менделеевская д.9 оф. 408</h3>  
+            </div>
+            <div class="col-sm-2 col-md-1 d-flex">
+                <h3 class="nav_item_1">СПб</h3>
+            </div>
+        </div>
+    </div>
+</nav>
+</header>
+    <section class="header">
+        <div class="nav_menu">
+        <div class="container">
+            <div class="row">
+            <div class="ARCHS col-3 offset-6 col-sm-2 col-md-2 offset-md-0 offset-sm-5">
+                <p><span class="p_logo">ARCH</span><br>indstrs</p>
+            </div>
+            <div class="header_list col-md-9 offset-sm-0 sm-12">
+                <ul class="list_style d-flex justify-content-end">
+                    <li class="list_item"><a href="#main_page"> Домой</a></li>
+                    <li class="list_item"><a href="#about_us">О нас</a></li>
+                    <li class="list_item"><a href="#projects">Проекты</a></li>
+                    <li class="list_item"><a href="#contacts">Контакты</a></li>
+                </ul>
+                </div>
+            </div>
+            </div>
+</section>
+<section class="main_page" id="main_page">
+    <div class="container">
+        <div class="row">
+<div>
+    <h1>
+        Строительство
+    </h1>
+    <div class="line">
+    </div>
+    <h2>
+        Строительные работы
+    </h2>
+    <h3>
+        Аренда оборудования
+    </h3>
+    
+</div>
+<div class="col-md-6 col-sm-6">
+<button class="main_button">
+        <a href="tel: +79013028282">Позвонить</a>
+    </button>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="about_us" id="about_us">
+<div class=row>
+<div class="container">
+    <img src="img/img_box.png" class="about_us_box">
+    <h2 class="about_us_title">О компании</h2>
+    <div class="about_us_content">
+    <p class="content_descr col-md-5">Дом - это неотъемлемая часть жизни человека – он должен быть уютным, красивым, отвечающим всем требованиям хозяина. Построить коробку с крышей очень просто, но ведь речь идет о строительстве домов. Сделать это хорошо, аккуратно и с душой могут далеко не все. </p>
+    
+    <img src="img/about_us_house.png" class="content_img  sm-offset-0 col-md-6 col-sm-12">
+</div>
+<div class="add_content">
+ <img src="img/about_us_design.png" class="design_img col-md-6 col-sm-12">
+ <div class="about_us_design d-flex">
+    <p class="design_descr content_descr col-md-9 col-sm-12">Заказывая постройку дома у нас Вам больше не надо беспокоиться не о чем: мы выполним весь спектр услуг от создания проекта дома,  до озеленения участка, ландшафтного дизайна и дизайна интерьера дома при этом мы учтем все Ваши пожелания.  Специалисты с отдела снабжения доставят необходимые материалы на объект. </p>
+</div>
+</div>
+</div>
+</section>
+
+<section class="projects" id="projects">
+    <div class=row>
+    <div class="container">
+        <img src="img/img_box.png" class="about_us_projects_box">
+        <h2 class="about_us_projects_title">Проекты</h2>
+        <h3 class="projects_title_1">Строительство
+заборов,<br>ворот</h3>
+        <div class="projects_line"></div>
+        <div class="projects_content">
+        <p class="projects_subtitle col-md-4 col-sm-12">Строительство заборов, подпорных стен, производство и монтаж ворот.</p>
+        <img src="img/projects_1.png" class="projects_1_img offset-md-1 col-md-6 col-sm-12"></div>
+
+
+    <h3 class="projects_title_2 col-md-6">Металлоконструкции
+изготовление<br>монтаж</h3>
+        <div class="projects_line_2"></div>
+        <div class="projects_content">
+        <p class="projects_subtitle_2 col-md-4 col-sm-12">Строительство заборов, подпорных стен, производство и монтаж ворот.</p>
+        <img src="img/projects_1.png" class="projects_2_img offset-md-1 col-md-6 col-sm-12"></div>
+    </div>
+    </div>
+</section>
+<section class="contacts" id="contacts">
+    <div class="row">
+        <div class="container">
+    <img src="img/img_box.png" class="contacts_box">
+    <h2 class="contacts">Контакты</h2>
+    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Aff36a11de81333cb5943b6b98bf3bb3903ee03b599751b4e9a14827b273466bb&amp;source=constructor" width="100%" height="700" frameborder="0" class="contacts_location"></iframe>
+        <div class="contacts_descr">
+    <div class="telephone d-flex">
+        <img src="img/nav_phone.png" class="contacts_phone">
+        <h3 class="contacts_tel">+7 901 302 82 82</h3>
+    </div>
+    <div class="mail d-flex">
+    <img src="img/email.png" class="contacts_mail">
+    <h3 class="contacts_email">9692852@rambler.ru</h3>  
+    </div>
+    <div class="adress d-flex">
+    <img src="img/nav_adress.png" class="contacts_adress">
+    <h3 class="contacts_loc">ул.Менделеевская <br>д.9 оф. 408</h3>  
+    </div>
+</div>
+</div>
+</div>
+</section>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="index.js"></script>
+</body>
+</html>
